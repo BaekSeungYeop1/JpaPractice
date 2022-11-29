2d063505eace590d257c8734deee044b7be6bd6c
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-11-29 23:18:06 +0900

    도메인 설계

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order; // 주문
+
+    private int orderPrice; // 주문 가격
+    private int count; //주문 수량
+}
