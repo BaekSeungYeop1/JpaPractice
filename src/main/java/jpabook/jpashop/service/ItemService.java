5b7216d31a9ced185837f9e54fad56979e305998
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-12-07 23:49:34 +0900

    상품 등록,목록,수정 기능 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -26,4 +26,12 @@ public class ItemService {
     public Item findOne(Long id){
         return itemRepository.findOne(id);
     }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price) {
+
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+    }
 }
